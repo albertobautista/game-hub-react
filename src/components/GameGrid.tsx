2e10207108f80c899d81368b23cfeca596e63be8
Alberto Bautista <dachb101@gmail.com>
2024-08-06 21:09:09 -0600

    add useGames custom hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,38 +1,12 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { AxiosError } from "axios";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-}
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const getGames = async () => {
-      try {
-        const response = await apiClient.get<FetchGamesResponse>("/games");
-        console.log(response.data);
-        setGames(response.data.results);
-      } catch (error) {
-        setError((error as AxiosError).message);
-      }
-    };
-    getGames();
-  }, []);
-
+  const { error, games, isLoading } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
+      {isLoading && <Text>Cargando...</Text>}
       <ul>
         {games.map((game) => (
           <li key={game.id}>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { AxiosError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+}
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const getGames = async () => {
+      try {
+        setIsLoading(true);
+        const response = await apiClient.get<FetchGamesResponse>("/games");
+        setGames(response.data.results);
+      } catch (error) {
+        setError((error as AxiosError).message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getGames();
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, isLoading };
+};
+
+export default useGames;
