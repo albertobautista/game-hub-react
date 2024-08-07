6d41bc15c3205d793dbd9484790c8d3c7b912f2a
Alberto Bautista <dachb101@gmail.com>
2024-08-06 20:45:40 -0600

    Add ColorSwitch component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image boxSize="60px" src="https://via.placeholder.com/100" alt="logo" />
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
