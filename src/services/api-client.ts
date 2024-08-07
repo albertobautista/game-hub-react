b281b07d529c522d3df7563e3ed2cef96dbc12a5
Alberto Bautista <dachb101@gmail.com>
2024-08-06 21:02:20 -0600

    use api key from var envs

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "650468153ed24626a34a93a0f4931949",
+    key: import.meta.env.VITE_API_KEY,
   },
 });
