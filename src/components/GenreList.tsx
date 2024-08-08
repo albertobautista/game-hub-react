89c6602fba203e675bbbe5fdaa52e84a21ab3435
Alberto Bautista <dachb101@gmail.com>
2024-08-07 18:05:08 -0600

    Add spinner in genres component

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,12 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { error, data: genres, isLoading } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner size="xl" />;
   return (
     <List>
       {genres?.map((genre) => (
