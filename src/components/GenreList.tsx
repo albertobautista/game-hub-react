bd7aa86ffb835912592bc9ed3e24868e01a882ac
Alberto Bautista <dachb101@gmail.com>
2024-09-04 19:46:23 -0600

    Use react-query on useGenres hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -25,7 +25,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: GenreListProps) => {
         Genres
       </Heading>
       <List>
-        {genres?.map((genre) => (
+        {genres?.results.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
