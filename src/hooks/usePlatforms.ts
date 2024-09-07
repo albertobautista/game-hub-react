2833753fe686572f61a006456008fdf2e88b7963
Alberto Bautista <dachb101@gmail.com>
2024-09-06 22:41:18 -0600

    Add ms dependency

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import ApiClient from "../services/api-client";
+import ms from "ms";
 
 export interface Platform {
   id: number;
@@ -12,7 +13,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: () => apiClient.getAll(),
-    staleTime: 24 * 60 * 60 * 1000, //24 hours
+    staleTime: ms("24h"),
   });
 
 export default usePlatforms;
